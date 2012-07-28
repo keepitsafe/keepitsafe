2dbd79a770a5ac7800e9021f98aad4c24fb2d384
felipecrp <felipecrp@gmail.com>
2012-07-28 10:18:13 -0300

    Change password to secret #2

---
+++ src/main/java/info/fcrp/keepitsafe/dao/SecretDAO.java
@@ -0,0 +1,7 @@
+package info.fcrp.keepitsafe.dao;
+
+import info.fcrp.keepitsafe.model.Secret;
+
+public interface SecretDAO extends GenericDAO<Secret> {
+
+}
---
+++ src/main/java/info/fcrp/keepitsafe/dao/SecretDAOImpl.java
@@ -0,0 +1,8 @@
+package info.fcrp.keepitsafe.dao;
+
+import info.fcrp.keepitsafe.model.Secret;
+
+public class SecretDAOImpl extends GenericDAOImpl<Secret> implements
+		SecretDAO {
+
+}
