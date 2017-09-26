2d50694a469833c33d66e7984e0d55cffd13706f
Felipe Curty <felipecrp@gmail.com>
2017-09-25 23:58:18 -0300

    implements keep in card decks

--- src/main/js/app/app.component.ts
+++ src/main/js/app/app.component.ts
@@ -7,8 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   keeps = [
-    { name: 'k1', description: 'Lorem ipsum dolor sit amet, consectetur' },
-    { name: 'k2', description: 'Lorem ipsum dolor sit amet, consectetur' },
+    { name: 'k1', description: 'Lorem ipsum dolor sit amet, consectetur, Lorem ipsum dolor sit amet, consectetu' },
+    { name: 'k2', description: 'Lorem ipsum dolor sit amet, consectetur a asd ad asd as lermasd asdjasdla' },
     { name: 'k3', description: 'Lorem ipsum dolor sit amet, consectetur' },
     { name: 'k4', description: 'Lorem ipsum dolor sit amet, consectetur' }
   ];
